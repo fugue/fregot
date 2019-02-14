19295cef26641f94bbb0ea3423680077c02a3082
Jasper Van der Jeugt <m@jaspervdj.be>
2019-02-14 17:55:46 +0100

    Insert modules into packages

--- lib/Fregot/Interpreter.hs
+++ lib/Fregot/Interpreter.hs
@@ -8,22 +8,31 @@ module Fregot.Interpreter
     , loadModule
     ) where
 
-import           Control.Lens            ((^.))
-import           Control.Lens.TH         (makeLenses)
-import           Control.Monad.Parachute (ParachuteT)
-import           Control.Monad.Trans     (liftIO)
-import qualified Data.IORef.Extended     as IORef
-import qualified Data.Text.IO            as T
-import           Fregot.Error            (Error)
-import qualified Fregot.Parser           as Parser
-import qualified Fregot.PrettyPrint      as PP
-import qualified Fregot.Sources          as Sources
-import qualified System.IO               as IO
+import           Control.Lens               ((^.))
+import           Control.Lens.TH            (makeLenses)
+import           Control.Monad              (foldM)
+import           Control.Monad.Parachute    (ParachuteT)
+import           Control.Monad.Trans        (liftIO)
+import qualified Data.HashMap.Strict        as HMS
+import           Data.IORef.Extended        (IORef)
+import qualified Data.IORef.Extended        as IORef
+import           Data.Maybe                 (fromMaybe)
+import qualified Data.Text.IO               as T
+import           Fregot.Error               (Error)
+import           Fregot.Interpreter.Package (Package)
+import qualified Fregot.Interpreter.Package as Package
+import qualified Fregot.Parser              as Parser
+import qualified Fregot.PrettyPrint         as PP
+import qualified Fregot.Sources             as Sources
+import           Fregot.Sources.SourceSpan  (SourceSpan)
+import qualified Fregot.Sugar               as Sugar
+import qualified System.IO                  as IO
 
 type InterpreterM a = ParachuteT Error IO a
 
 data Handle = Handle
-    { _sources :: !Sources.Handle
+    { _sources  :: !Sources.Handle
+    , _packages :: !(IORef (HMS.HashMap Sugar.PackageName Package))
     }
 
 $(makeLenses ''Handle)
@@ -33,14 +42,38 @@ withHandle
     -> (Handle -> InterpreterM a)
     -> InterpreterM a
 withHandle _sources f = do
+    _packages <- liftIO $ IORef.newIORef HMS.empty
     f Handle {..}
 
 loadModule :: Handle -> FilePath -> InterpreterM ()
 loadModule h path = do
+    -- Read the source code and parse the module.
     input <- liftIO $ T.readFile path
     liftIO $ IORef.atomicModifyIORef_ (h ^. sources) $
         Sources.insert sourcep input
     modul <- Parser.lexAndParse Parser.parseModule sourcep input
     liftIO $ PP.hPutSemDoc IO.stdout $ PP.pretty modul
+
+    -- Insert the module into the packages system.
+    insertModule h modul
   where
     sourcep = Sources.FileInput path
+
+-- | TODO(jaspervdj): This will require a lock if we concurrently load modules.
+insertModule :: Handle -> Sugar.Module SourceSpan -> InterpreterM ()
+insertModule h modul = do
+    -- Lookup an existing package or create a new one if necessary.
+    package0 <- fromMaybe (Package.empty pkgname) . HMS.lookup pkgname <$>
+        liftIO (IORef.readIORef (h ^. packages))
+
+    -- One by one, add the rules in the sugared module to the package.
+    package1 <- foldM
+        (\pkg rule -> Package.insert (modul ^. Sugar.moduleImports) rule pkg)
+        package0
+        (modul ^. Sugar.modulePolicy)
+
+    -- Save the modified package.
+    liftIO $ IORef.atomicModifyIORef_ (h ^. packages) $
+        HMS.insert pkgname package1
+  where
+    pkgname = modul ^. Sugar.modulePackage
--- lib/Fregot/Interpreter/Package.hs
+++ lib/Fregot/Interpreter/Package.hs
@@ -1,13 +1,58 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Fregot.Interpreter.Package
-    ( Package (..), packageName
+    ( Package (..), packageName, packageRules
+    , CompiledRule
+    , RuleDefinition (..), ruleDefImports, ruleDefRule
+
+    , empty
+    , insert
+    , lookup
     ) where
 
-import           Control.Lens.TH (makeLenses)
+import           Control.Lens              ((%~), (&), (^.))
+import           Control.Lens.TH           (makeLenses)
+import           Control.Monad.Parachute
+import qualified Data.HashMap.Strict       as HMS
+import           Data.Maybe                (fromMaybe)
+import           Fregot.Error              (Error)
+import           Fregot.Sources.SourceSpan (SourceSpan)
 import           Fregot.Sugar
+import           Prelude                   hiding (lookup)
 
 data Package = Package
-    { _packageName :: !PackageName
+    { _packageName  :: !PackageName
+    , _packageRules :: !(HMS.HashMap Var CompiledRule)
+    } deriving (Show)
+
+type CompiledRule = [RuleDefinition]
+
+data RuleDefinition = RuleDefinition
+    { _ruleDefImports :: ![Import SourceSpan]
+    , _ruleDefRule    :: !(Rule SourceSpan)
     } deriving (Show)
 
 $(makeLenses ''Package)
+$(makeLenses ''RuleDefinition)
+
+empty :: PackageName -> Package
+empty name = Package
+    { _packageName  = name
+    , _packageRules = HMS.empty
+    }
+
+-- | Add a new rule.
+insert
+    :: Monad m
+    => [Import SourceSpan]
+    -> Rule SourceSpan
+    -> Package
+    -> ParachuteT Error m Package
+insert imports rule package = return $
+    -- | TODO(jaspervdj): Error out if arity is wrong.
+    package & packageRules %~ HMS.insertWith (++) rname [rdef]
+  where
+    rname = rule ^. ruleHead . ruleName
+    rdef  = RuleDefinition imports rule
+
+lookup :: Var -> Package -> [RuleDefinition]
+lookup var pkg = fromMaybe [] $ HMS.lookup var (pkg ^. packageRules)
