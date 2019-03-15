49c6a532317fc0aa211cef95278d5e1d64378af0
Jasper Van der Jeugt <jasper@fugue.co>
2019-03-15 18:03:14 +0100

    Compute output vars

--- lib/Fregot/Prepare/Vars.hs
+++ lib/Fregot/Prepare/Vars.hs
@@ -1,7 +1,11 @@
-{-# LANGUAGE DeriveFunctor   #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DeriveFunctor              #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase                 #-}
+{-# LANGUAGE TemplateHaskell            #-}
 module Fregot.Prepare.Vars
-    ( Dir (..)
+    (
+      {-
+      Dir (..)
     , Vars (..), varsIn, varsOut
 
     , Scope (..), locals, globals, builtins
@@ -10,16 +14,93 @@ module Fregot.Prepare.Vars
     , freeRuleBody
     , freeStatement
     , freeTerm
+    -}
+
+      Safe (..)
+    , Arities
+    , ovRuleBody
     ) where
 
-import           Control.Lens                (foldMapOf, (.~), (^.))
-import           Control.Lens.TH             (makeLenses)
-import           Data.Hashable               (Hashable)
-import qualified Data.HashMap.Strict         as HMS
-import qualified Data.HashSet                as HS
-import qualified Data.List.NonEmpty.Extended as NonEmpty
-import qualified Fregot.Eval.Builtins        as Builtins
+import           Control.Lens          ((^.))
+import           Data.Hashable         (Hashable)
+import qualified Data.HashSet.Extended as HS
+import           Data.List             (foldl')
 import           Fregot.Prepare.Ast
+import           Fregot.Prepare.Lens
+
+newtype Safe v = Safe (HS.HashSet v) deriving (Eq, Monoid, Semigroup, Show)
+
+markSafe :: (Eq v, Hashable v) => v -> Safe v
+markSafe = Safe . HS.singleton
+
+markTermSafe :: Term a -> Safe Var
+markTermSafe t = Safe (HS.toHashSetOf (termCosmosNoClosures . termVars) t)
+
+isSafe :: (Eq v, Hashable v) => v -> Safe v -> Bool
+isSafe v (Safe s) = HS.member v s
+
+type Arities = Function -> Int
+
+ovRuleBody :: Arities -> Safe Var -> RuleBody a -> Safe Var
+ovRuleBody arities safe@(Safe initial) body =
+    Safe $ total `HS.difference` initial
+  where
+    Safe total = foldl'
+        (\acc lit -> let ov = ovLiteral arities acc lit in acc <> ov)
+        safe
+        body
+
+ovLiteral :: Arities -> Safe Var -> Literal a -> Safe Var
+ovLiteral arities safe lit
+    | lit ^. literalNegation = mempty
+    | otherwise              =
+        ovStatement arities safe (lit ^. literalStatement)
+
+ovStatement :: Arities -> Safe Var -> Statement a -> Safe Var
+ovStatement arities safe = \case
+    TermS t       -> ovTerm arities safe t
+    AssignS _ v t -> markSafe v <> ovTerm arities safe t
+    UnifyS _ x y  -> ovUnify arities safe x y
+
+ovTerm :: Arities -> Safe Var -> Term a -> Safe Var
+ovTerm arities safe (RefT _ x k) =
+    ovTerm arities safe x <>
+    markTermSafe k
+
+ovTerm arities safe (CallT _ function args) =
+    let arity = arities function in
+    foldMap (ovTerm arities safe) (take arity args) <>
+    foldMap markTermSafe (drop arity args)
+
+ovTerm _arities _safe (VarT _ _) = mempty
+
+ovTerm _arities _safe (ScalarT _ _) = mempty
+
+ovTerm arities safe (ArrayT _ xs) = foldMap (ovTerm arities safe) xs
+
+ovTerm arities safe (SetT _ xs) = foldMap (ovTerm arities safe) xs
+
+ovTerm arities safe (ObjectT _ xs) =
+    foldMap (\(k, v) -> ovTerm arities safe k <> ovTerm arities safe v) xs
+
+ovTerm _arities _safe (ArrayCompT _ _ _) = mempty
+ovTerm _arities _safe (SetCompT _ _ _) = mempty
+ovTerm _arities _safe (ObjectCompT _ _ _ _) = mempty
+
+ovUnify :: Arities -> Safe Var -> Term a -> Term a -> Safe Var
+ovUnify _arities safe (VarT _ alpha) (VarT _ beta)
+    | isSafe alpha safe = markSafe beta
+    | isSafe beta  safe = markSafe alpha
+    | otherwise         = mempty  -- TODO(jaspervdj): Mark as unknown.
+
+ovUnify arities safe (VarT _ alpha) y = markSafe alpha <> ovTerm arities safe y
+ovUnify arities safe x (VarT _ beta)  = markSafe beta <> ovTerm arities safe x
+
+ovUnify arities safe x y =
+    ovTerm arities safe x <> ovTerm arities safe y
+
+
+{-
 
 -- NOTE(jaspervdj): This is where we would have a spot to push down and
 -- propagate up types as well.
@@ -149,3 +230,5 @@ freeObject :: Scope Var -> Dir ty -> Object a -> Vars Var a
 freeObject scope dir object = foldMap
     (\(k, t) -> freeTerm scope dir k <> freeTerm scope dir t)
     object
+
+-}
--- tests/hs/Fregot/Prepare/Vars/Tests.hs
+++ tests/hs/Fregot/Prepare/Vars/Tests.hs
@@ -2,39 +2,52 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Fregot.Prepare.Vars.Tests where
 
-import           Control.Lens                ((&), (.~))
-import qualified Data.HashMap.Strict         as HMS
-import           Data.List.NonEmpty.Extended (NonEmpty)
 import           Fregot.Prepare.Ast
 import           Fregot.Prepare.Vars
-import qualified Test.Tasty                  as Tasty
-import           Test.Tasty.HUnit            ((@?=))
-import qualified Test.Tasty.HUnit            as Tasty
+import qualified Test.Tasty          as Tasty
+import           Test.Tasty.HUnit    ((@?=))
+import qualified Test.Tasty.HUnit    as Tasty
 
 tests :: Tasty.TestTree
 tests = Tasty.testGroup "Fregot.Prepare.Vars.Tests"
-    [ Tasty.testCase "freeRuleBody 01" $
-        freeRuleBody emptyScope
-            [ literal $ UnifyS 0 (VarT 0 "a") (VarT 0 "b")
+    [ Tasty.testCase "ovRuleBody 01" $
+        ovRuleBody (const 2) mempty
+            [ literal $ TermS $ RefT 0 (var "a") (var "b")
             ] @?=
-        mkVars [("a", [0 :: Int]) ,("b", [0])] []
-    , Tasty.testCase "freeRuleBody 02" $
-        freeRuleBody emptyScope
-            [ literal $ UnifyS 0 (VarT 0 "b") (ScalarT 0 (String "wat"))
+        Safe ["b"]
+    , Tasty.testCase "ovRuleBody 02" $
+        ovRuleBody (const 2) (Safe ["a"])
+            [ literal $ UnifyS 0 (var "a") (var "b")
             ] @?=
-        mkVars [] ["b"]
-        {-
-    , Tasty.testCase "freeRuleBody 03" $
-        freeRuleBody emptyScope
-            [ literal $ UnifyS 0 (VarT 0 "a") (ScalarT 0 (String "wat"))
-            , literal $ UnifyS 0 (VarT 0 "b") (VarT 0 "a")
+        Safe ["b"]
+    , Tasty.testCase "ovRuleBody 03" $
+        ovRuleBody (const 2) (Safe ["b"])
+            [ literal $ UnifyS 0 (var "a") (var "b")
             ] @?=
-        mkVars [] ["b"]
-        -}
+        Safe ["a"]
+    , Tasty.testCase "ovRuleBody 04" $
+        ovRuleBody (const 2) mempty
+            [ literal $ UnifyS 0 (var "a") (num 1)
+            , literal $ UnifyS 0 (var "b") (var "a")
+            ] @?=
+        Safe ["a", "b"]
+    , Tasty.testCase "ovRuleBody 05" $
+        ovRuleBody (const 2) mempty
+            [ literal $ TermS $
+                CallT 0 (NamedFunction ["add"]) [var "a", var "b", var "x"]
+            ] @?=
+        Safe ["x"]
+    , Tasty.testCase "ovRuleBody 06" $
+        ovRuleBody (const 2) mempty
+            [ literal $ UnifyS 0
+                (var "x")
+                (CallT 0 (NamedFunction ["add"]) [var "a", var "b"])
+            ] @?=
+        Safe ["x"]
     ]
-  where
-    mkVars :: [(Var, NonEmpty Int)] -> [Var] -> Vars Var Int
-    mkVars ins outs =
-        mempty
-            & varsIn .~ HMS.fromList ins
-            & varsOut .~ HMS.fromList (zip outs (repeat ()))
+
+var :: Var -> Term Int
+var = VarT 0
+
+num :: Int -> Term Int
+num = ScalarT 0 . Number . fromIntegral
