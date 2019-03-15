d721376e4b7419d7fbaadb59e0baebe891fbb396
Jasper Van der Jeugt <jasper@fugue.co>
2019-03-15 20:42:13 +0100

    "Refactor"

--- tests/hs/Fregot/Prepare/Vars/Tests.hs
+++ tests/hs/Fregot/Prepare/Vars/Tests.hs
@@ -11,38 +11,37 @@ import qualified Test.Tasty.HUnit    as Tasty
 
 tests :: Tasty.TestTree
 tests = Tasty.testGroup "Fregot.Prepare.Vars.Tests"
-    [ Tasty.testCase "ovRuleBody 01" $
-        ovRuleBody (const 2) mempty
-            [ lit $ TermS $ RefT 0 (var "a") (var "b")
-            ] @?=
+    [ tests_ovRuleBody
+    ]
+
+tests_ovRuleBody :: Tasty.TestTree
+tests_ovRuleBody = Tasty.testGroup "ovRuleBody"
+    [ Tasty.testCase "01" $ ovRuleBody (const 2) mempty
+        [ lit $ TermS $ RefT 0 (var "a") (var "b")
+        ] @?=
         Safe ["b"]
-    , Tasty.testCase "ovRuleBody 02" $
-        ovRuleBody (const 2) (Safe ["a"])
-            [ lit $ UnifyS 0 (var "a") (var "b")
-            ] @?=
+    , Tasty.testCase "02" $ ovRuleBody (const 2) (Safe ["a"])
+        [ lit $ UnifyS 0 (var "a") (var "b")
+        ] @?=
         Safe ["b"]
-    , Tasty.testCase "ovRuleBody 03" $
-        ovRuleBody (const 2) (Safe ["b"])
-            [ lit $ UnifyS 0 (var "a") (var "b")
-            ] @?=
+    , Tasty.testCase "03" $ ovRuleBody (const 2) (Safe ["b"])
+        [ lit $ UnifyS 0 (var "a") (var "b")
+        ] @?=
         Safe ["a"]
-    , Tasty.testCase "ovRuleBody 04" $
-        ovRuleBody (const 2) mempty
-            [ lit $ UnifyS 0 (var "a") (num 1)
-            , lit $ UnifyS 0 (var "b") (var "a")
-            ] @?=
+    , Tasty.testCase "04" $ ovRuleBody (const 2) mempty
+        [ lit $ UnifyS 0 (var "a") (num 1)
+        , lit $ UnifyS 0 (var "b") (var "a")
+        ] @?=
         Safe ["a", "b"]
-    , Tasty.testCase "ovRuleBody 05" $
-        ovRuleBody (const 2) mempty
-            [ lit $ TermS $
-                CallT 0 (NamedFunction ["add"]) [var "a", var "b", var "x"]
-            ] @?=
+    , Tasty.testCase "05" $ ovRuleBody (const 2) mempty
+        [ lit $ TermS $
+            CallT 0 (NamedFunction ["add"]) [var "a", var "b", var "x"]
+        ] @?=
         Safe ["x"]
-    , Tasty.testCase "ovRuleBody 06" $
-        ovRuleBody (const 2) mempty
-            [ lit $ UnifyS 0
-                (var "x")
-                (CallT 0 (NamedFunction ["add"]) [var "a", var "b"])
-            ] @?=
+    , Tasty.testCase "06" $ ovRuleBody (const 2) mempty
+        [ lit $ UnifyS 0
+            (var "x")
+            (CallT 0 (NamedFunction ["add"]) [var "a", var "b"])
+        ] @?=
         Safe ["x"]
     ]
