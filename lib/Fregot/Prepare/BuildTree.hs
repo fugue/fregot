e245327256d5cc8cdcd3afc8e8ee8b978df9b9dd
Jasper Van der Jeugt <jasper@fugue.co>
2019-12-20 18:44:27 +0100

    Add support for loading JSON/YAML files as data

---
+++ lib/Fregot/Prepare/BuildTree.hs
@@ -0,0 +1,57 @@
+--------------------------------------------------------------------------------
+-- | Helper module for building rule trees from JSON and YAML documents.
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Fregot.Prepare.BuildTree
+    ( BuildTree (..)
+    , toTree
+    , toTerm
+    ) where
+
+import           Control.Lens              (review)
+import qualified Data.List                 as L
+import           Fregot.Names
+import           Fregot.Prepare.Ast
+import           Fregot.Prepare.Package    (PreparedRule)
+import           Fregot.Sources.SourceSpan (SourceSpan)
+import qualified Fregot.Tree               as Tree
+
+data BuildTree
+    = BuildSingleton (Term SourceSpan)
+    | BuildTree      SourceSpan [(SourceSpan, Var, BuildTree)]
+
+toTree
+    :: PackageName -> [(SourceSpan, Var, BuildTree)] -> Tree.Tree PreparedRule
+toTree pkgname = L.foldl' Tree.union Tree.empty . map toRuleOrTree
+  where
+    toRuleOrTree (loc, var, BuildSingleton t) = Tree.singleton
+        (review varFromKey var)
+        Rule
+            { _rulePackage = pkgname
+            , _ruleName    = var
+            , _ruleKey     = review qualifiedVarFromKey (pkgname, var)
+            , _ruleAnn     = loc
+            , _ruleKind    = CompleteRule
+            , _ruleInfo    = ()
+            , _ruleDefault = Nothing
+            , _ruleDefs    = pure RuleDefinition
+                { _ruleDefName    = var
+                , _ruleDefImports = mempty
+                , _ruleDefAnn     = loc
+                , _ruleArgs       = Nothing
+                , _ruleIndex      = Nothing
+                , _ruleValue      = Just t
+                , _ruleBodies     = []
+                , _ruleElses      = []
+                }
+            }
+
+    toRuleOrTree (_, var, BuildTree _ t) =
+        Tree.parent [(var, toTree (pkgname <> mkPackageName [unVar var]) t)]
+
+toTerm :: BuildTree -> Term SourceSpan
+toTerm (BuildSingleton t) = t
+toTerm (BuildTree loc children) = ObjectT loc $
+    [ (ScalarT l (String $ unVar v), toTerm child)
+    | (l, v, child) <- children
+    ]
---
+++ tests/rego/yaml.rego
@@ -0,0 +1,7 @@
+package fregot.tests.yaml
+
+import data.yaml_policy
+
+test_yaml_policy {
+  yaml_policy.filename == "sqlite.c"
+}
