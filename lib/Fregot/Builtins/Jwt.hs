bfbec123b49a1ba61202fa81807e351d05ece575
Jasper Van der Jeugt <jasper@fugue.co>
2020-05-01 23:34:32 +0200

    Add decode_verify

--- lib/Fregot/Builtins/Jwt.hs
+++ lib/Fregot/Builtins/Jwt.hs
@@ -1,25 +1,32 @@
 -- | JWT-related builtins.
 {-# LANGUAGE DerivingVia       #-}
 {-# LANGUAGE GADTs             #-}
+{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Fregot.Builtins.Jwt
     ( builtins
     ) where
 
-import           Control.Lens             ((^.), (^..))
-import           Control.Monad.Except     (ExceptT, runExceptT)
+import           Control.Lens             (set, (^.), (^..))
+import           Control.Monad.Except     (ExceptT, catchError, runExceptT)
 import           Control.Monad.Trans      (liftIO)
-import qualified Crypto.JOSE.Error        as Jose
 import qualified Crypto.JWT               as Jwt
-import           Data.Aeson               as Aeson
+import qualified Data.Aeson               as Aeson
+import qualified Data.Aeson.Types         as Aeson
 import qualified Data.ByteString.Base16   as Base16
-import qualified Data.ByteString.Base64 as Base64
+import qualified Data.ByteString.Base64   as Base64
 import           Data.Functor.Identity    (Identity)
 import qualified Data.HashMap.Strict      as HMS
+import           Data.Maybe               (isJust)
+import qualified Data.Set                 as S
 import qualified Data.Text.Encoding       as T
 import qualified Data.Text.Lazy           as TL
 import qualified Data.Text.Lazy.Encoding  as TL
+import           Data.Time                (UTCTime)
+import qualified Data.Time                as Time
 import           Fregot.Builtins.Internal
+import           Fregot.Builtins.Time     (nsToUtc)
+import           Fregot.Eval.Value        (emptyObject)
 import           Fregot.Names             hiding (Key)
 import           Fregot.Types.Builtins    ((🡒))
 import qualified Fregot.Types.Builtins    as Ty
@@ -27,8 +34,9 @@ import qualified Fregot.Types.Internal    as Ty
 
 builtins :: Builtins IO
 builtins = HMS.fromList
-    [ (NamedFunction (QualifiedName "io.jwt.encode_sign"), builtin_encode_sign)
-    , (NamedFunction (QualifiedName "io.jwt.decode"), builtin_decode)
+    [ (NamedFunction (QualifiedName "io.jwt.encode_sign"),   builtin_encode_sign)
+    , (NamedFunction (QualifiedName "io.jwt.decode"),        builtin_decode)
+    , (NamedFunction (QualifiedName "io.jwt.decode_verify"), builtin_decode_verify)
     ]
 
 newtype Headers = Headers (Jwt.JWSHeader ())
@@ -44,25 +52,24 @@ instance Aeson.ToJSON Headers where
 
 newtype Payload = Payload Jwt.ClaimsSet
     deriving FromVal via (Json Jwt.ClaimsSet)
+    deriving ToVal via (Json Jwt.ClaimsSet)
 
 newtype Key = Key Jwt.JWK
     deriving FromVal via (Json Jwt.JWK)
 
-runJwsM :: ExceptT Jose.Error IO a -> BuiltinM a
+runJwsM :: ExceptT Jwt.JWTError IO a -> BuiltinM a
 runJwsM mx = liftIO (runExceptT mx) >>=
     either (\e -> throwString $ "JWS error: " ++ show e) pure
 
 builtin_encode_sign :: Applicative m => Builtin m
-builtin_encode_sign = Builtin
-    (In (In (In Out)))
+builtin_encode_sign = Builtin (In (In (In Out)))
     (Ty.any 🡒 Ty.any 🡒 Ty.any 🡒 Ty.out (Ty.string)) $ pure $
     \(Cons (Headers header) (Cons (Payload payload) (Cons (Key key) Nil))) -> do
     signed <- runJwsM $ Jwt.signClaims key header payload
     pure . TL.toStrict . TL.decodeUtf8 $! Jwt.encodeCompact signed
 
 builtin_decode :: Applicative m => Builtin m
-builtin_decode = Builtin
-    (In Out)
+builtin_decode = Builtin (In Out)
     (Ty.string 🡒 Ty.out (Ty.arrayOf Ty.unknown)) $ pure $
     \(Cons t Nil) -> do
     -- Decode the compact representation.
@@ -91,3 +98,52 @@ builtin_decode = Builtin
             pure val
         | otherwise =
             throwString "Internal error obtaining JWT payload"
+
+data Constraints = Constraints Jwt.JWK (Maybe UTCTime) Jwt.JWTValidationSettings
+    deriving FromVal via (Json Constraints)
+
+parseKey :: Aeson.Object -> Aeson.Parser Jwt.JWK
+parseKey obj =
+    (do
+        secret <- obj Aeson..: "secret"
+        pure $! Jwt.fromOctets (T.encodeUtf8 secret))
+
+parseTime :: Aeson.Object -> Aeson.Parser (Maybe UTCTime)
+parseTime obj = fmap (fmap nsToUtc) $! obj Aeson..:? "time"
+
+parseValidationSettings
+    :: Aeson.Object -> Aeson.Parser Jwt.JWTValidationSettings
+parseValidationSettings obj = do
+    alg <- maybe id (\a -> set Jwt.validationSettingsAlgorithms (S.singleton a))
+        <$> obj Aeson..:? "alg"
+    iss <- maybe id (\i -> set Jwt.jwtValidationSettingsIssuerPredicate (== i))
+        <$> obj Aeson..:? "iss"
+    aud <- maybe id
+            (\a -> set Jwt.jwtValidationSettingsAudiencePredicate (== a))
+        <$> obj Aeson..:? "aud"
+    pure . alg . iss . aud $! Jwt.defaultJWTValidationSettings (const True)
+
+instance Aeson.FromJSON Constraints where
+    parseJSON = Aeson.withObject "FromJSON Constraints" $ \obj -> Constraints
+        <$> parseKey obj
+        <*> parseTime obj
+        <*> parseValidationSettings obj
+
+builtin_decode_verify :: Applicative m => Builtin m
+builtin_decode_verify = Builtin (In (In Out))
+    (Ty.string 🡒 Ty.any 🡒 Ty.out (Ty.arrayOf Ty.unknown)) $ pure $
+    \(Cons t (Cons (Constraints jwk mbTime valSettings) Nil)) -> do
+    jws <- runJwsM . Jwt.decodeCompact . TL.encodeUtf8 $! TL.fromStrict t
+    time <- liftIO $ maybe Time.getCurrentTime pure mbTime
+    mbClaims <- runJwsM $ catchError
+        (Just <$> Jwt.verifyClaimsAt valSettings jwk time jws)
+        (\_err -> pure Nothing)
+    headers <- case jws ^.. Jwt.signatures of
+        [sig] -> pure $! sig ^. Jwt.header
+        _     -> throwString $
+            "JWT tokens with multiple signatures are currently unsupported"
+    pure
+        [ toVal (isJust mbClaims)
+        , toVal $! Headers headers
+        , maybe emptyObject (toVal . Payload) mbClaims
+        ]
--- tests/rego/builtins-10.rego
+++ tests/rego/builtins-10.rego
@@ -1,20 +1,27 @@
 # JWT tests.
 package fregot.tests.builtins_10
 
+secret_password = "hunter2 hunter2 hunter2 hunter2 hunter2"
+
 example_01 = {
   "header": {"typ": "JWT", "alg": "HS256"},
   "payload": {"iss": "alice"},
   "key": {
     "kty": "oct",
-    "k": base64.encode("hunter2 hunter2 hunter2 hunter2 hunter2")
+    "k": base64.encode(secret_password)
   }
 }
 
 test_round_trip_01 {
   jwt = io.jwt.encode_sign(example_01.header, example_01.payload, example_01.key)
-  [header, payload, _] = io.jwt.decode(jwt)
-  header == example_01.header
-  payload == example_01.payload
+  io.jwt.decode(jwt, [example_01.header, example_01.payload, _])
+  io.jwt.decode_verify(jwt, {"secret": secret_password},
+      [true, example_01.header, example_01.payload])
+}
+
+test_round_trip_02 {
+  jwt = io.jwt.encode_sign(example_01.header, example_01.payload, example_01.key)
+  io.jwt.decode_verify(jwt, {"secret": "abc123"}, [false, _, _])
 }
 
 test_decode_sig_01 {
