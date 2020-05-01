74a37a197e4730eb3ad259a0c13b9d672d87b857
Jasper Van der Jeugt <jasper@fugue.co>
2020-05-01 13:10:31 +0200

    Use jose library

--- lib/Fregot/Builtins/Jwt.hs
+++ lib/Fregot/Builtins/Jwt.hs
@@ -6,43 +6,45 @@ module Fregot.Builtins.Jwt
     ( builtins
     ) where
 
+import           Control.Monad.Except     (runExceptT)
+import           Control.Monad.Trans      (liftIO)
+import qualified Crypto.JOSE.Error        as Jose
+import qualified Crypto.JWT               as Jwt
 import           Data.Aeson               as Aeson
 import qualified Data.HashMap.Strict      as HMS
-import qualified Data.Text                as T
+import qualified Data.Text.Lazy           as TL
+import qualified Data.Text.Lazy.Encoding  as TL
 import           Fregot.Builtins.Internal
 import           Fregot.Names             hiding (Key)
 import           Fregot.Types.Builtins    ((🡒))
 import qualified Fregot.Types.Builtins    as Ty
 import qualified Fregot.Types.Internal    as Ty
-import qualified Web.JWT                  as Jwt
 
 builtins :: Builtins IO
 builtins = HMS.fromList
     [ (NamedFunction (QualifiedName "io.jwt.encode_sign"), builtin_encode_sign)
     ]
 
-newtype Headers = Headers Jwt.JOSEHeader
-    deriving FromVal via (Json Jwt.JOSEHeader)
+newtype Headers = Headers (Jwt.JWSHeader ())
+    deriving FromVal via (Json Headers)
 
-newtype Payload = Payload Jwt.JWTClaimsSet
-    deriving FromVal via (Json Jwt.JWTClaimsSet)
+instance Aeson.FromJSON Headers where
+    parseJSON = Aeson.withObject "FromJSON Headers" $ \obj ->
+        Headers <$> Jwt.parseParams (Just obj) Nothing
 
-newtype Key = Key Jwt.Signer
-    deriving FromVal via (Json Key)
+newtype Payload = Payload Jwt.ClaimsSet
+    deriving FromVal via (Json Jwt.ClaimsSet)
 
-instance Aeson.FromJSON Key where
-    parseJSON = Aeson.withObject "FromJSON Key" $ \obj -> do
-        kty <- obj Aeson..: "kty"
-        case (kty :: T.Text) of
-            "oct" -> do
-                k <- obj Aeson..: "k"
-                pure . Key $! Jwt.hmacSecret k
-            _ -> fail "Expected 'oct' key type"
+newtype Key = Key Jwt.JWK
+    deriving FromVal via (Json Jwt.JWK)
 
 builtin_encode_sign :: Applicative m => Builtin m
 builtin_encode_sign = Builtin
     (In (In (In Out)))
     (Ty.any 🡒 Ty.any 🡒 Ty.any 🡒 Ty.out (Ty.string)) $ pure $
-    \(Cons (Headers header) (Cons (Payload payload) (Cons (Key key) Nil))) ->
-    let _ = Jwt.encodeSigned key header payload in
-    pure $! Jwt.encodeSigned key header mempty
+    \(Cons (Headers header) (Cons (Payload payload) (Cons (Key key) Nil))) -> do
+    errOrSigned <- liftIO $ runExceptT $ Jwt.signClaims key header payload
+    case errOrSigned of
+        Left err -> throwString (show (err :: Jose.Error))
+        Right signed ->
+            pure . TL.toStrict . TL.decodeUtf8 $! Jwt.encodeCompact signed
